1c20e1214dd32a8d1ebc03bc8135dfe721fb234a
Jordan Curzon <curzonj@gmail.com>
2013-07-26 23:04:44 -0700

    make logging customizable

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,5 @@
 default['12factor_haproxy']['port'] = 8080
-default['12factor_haproxy']['config_cookbook'] = 'haproxy'
+default['12factor_haproxy']['logging']['host'] = '127.0.0.1'
+default['12factor_haproxy']['logging']['port'] = '514'
+default['12factor_haproxy']['config_cookbook'] = '12factor_haproxy'
 default['12factor_haproxy']['config_template'] = "haproxy-app_lb.cfg.erb"
